08d4bee11c35c611b94fb2839929509ac70fd987
Gabyani Sharma <gabyanisharma@gmail.com>
2026-05-23 18:56:51 +0545

    Resolve merge conflicts between local files and remote branch

---
+++ app/routes/analytics_routes.py
@@ -0,0 +1,25 @@
+from flask import Blueprint
+from app.routes.user_routes import login_required
+from app.controllers.analytics_controller import (
+    meeting_history, analytics_data, export_history
+)
+
+analytics_bp = Blueprint('analytics', __name__, url_prefix='/analytics')
+
+
+@analytics_bp.route('/')
+@login_required
+def history():
+    return meeting_history()
+
+
+@analytics_bp.route('/export')
+@login_required
+def export():
+    return export_history()
+
+
+@analytics_bp.route('/data')
+@login_required
+def data():
+    return analytics_data()
