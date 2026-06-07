f5cac7472e9f8369ce88e39c2cfba52bb6dd62b4
kripa122 <kripaadhikari722@gmail.com>
2026-06-07 12:23:09 +0545

    conflict solved and deleted cache

--- app/routes/place_routes.py
+++ app/routes/place_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, url_for
+from app.controllers.place_controller import restaurants, api_filter_restaurants
 from app.routes.user_routes import login_required
 
 place_bp = Blueprint('place', __name__, url_prefix='/place')
@@ -6,4 +7,4 @@ place_bp = Blueprint('place', __name__, url_prefix='/place')
 @place_bp.route('/saved')
 @login_required
 def saved():
-    return render_template('place/saved.html')
+    return redirect(url_for('user.dashboard'))  # placeholder§
