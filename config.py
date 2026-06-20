642055523c3b299c1d4fac8faa5734c4eaf78e55
Gabyani <gabyanisharma@gmail.com>
2026-06-20 21:38:04 +0545

    .

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MYSQL_HOST = os.getenv('DB_HOST', 'localhost')
-    MYSQL_USER = os.getenv('DB_USER', 'root')
+    MYSQL_HOST = os.getenv('DB_HOST')
+    MYSQL_USER = os.getenv('DB_USER')
     MYSQL_PASSWORD = os.getenv('DB_PASSWORD')
     MYSQL_DB = os.getenv('DB_NAME')
